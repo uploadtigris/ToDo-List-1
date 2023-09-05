1422af7cfc590698a0343655568c9e2da35b09c0
Tigris Mendez <70561045+uploadtigris@users.noreply.github.com>
2023-09-05 00:10:50 -0500

    src files uploaded

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./styles.css"
+import { TodoList } from "./TodoList"
+
+
+export default function App(){
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  // saves items to local storage
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false},
+      ]
+    })
+  }
+
+  //toggle the checkboxes 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  //delete todos
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      //filter includes all todos except for the one to remove.
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    </>
+  ) 
+}
